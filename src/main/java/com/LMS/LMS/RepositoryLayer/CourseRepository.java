3315096c209d2cbdfd469b3d5be553947e99faa2
Menna Saad <122990205+ms2912003@users.noreply.github.com>
2024-12-10 20:18:57 +0200

    Add files via upload

---
+++ src/main/java/com/LMS/LMS/RepositoryLayer/CourseRepository.java
@@ -0,0 +1,7 @@
+package RepositoryLayer;
+
+import ModelLayer.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+}
