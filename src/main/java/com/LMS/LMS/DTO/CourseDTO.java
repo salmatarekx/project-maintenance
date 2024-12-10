394f7e529efa256cfdaf9ed243cd25655d966970
Menna Saad <122990205+ms2912003@users.noreply.github.com>
2024-12-10 20:18:05 +0200

    Add files via upload

---
+++ src/main/java/com/LMS/LMS/DTO/CourseDTO.java
@@ -0,0 +1,53 @@
+package DTO;
+import ModelLayer.User;
+
+import java.util.List;
+
+public class CourseDTO {
+    private String title;
+    private String description;
+    private String duration;
+    private List<String> mediaFiles;
+    private User instructor;
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public List<String> getMediaFiles() {
+        return mediaFiles;
+    }
+
+    public void setMediaFiles(List<String> mediaFiles) {
+        this.mediaFiles = mediaFiles;
+    }
+
+    public User getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(User instructor) {
+        this.instructor = instructor;
+    }
+}
