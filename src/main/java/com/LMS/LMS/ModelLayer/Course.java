16bd36ea7af596237191dfb8b46f24a727238316
Menna Saad <122990205+ms2912003@users.noreply.github.com>
2024-12-10 20:18:27 +0200

    Add files via upload

---
+++ src/main/java/com/LMS/LMS/ModelLayer/Course.java
@@ -0,0 +1,70 @@
+package ModelLayer;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String description;
+    private String duration;
+
+    @ElementCollection
+    private List<String> mediaFiles; // URLs or paths to course materials
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id", nullable = false)
+    private User instructor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public List<String> getMediaFiles() {
+        return mediaFiles;
+    }
+
+    public void setMediaFiles(List<String> mediaFiles) {
+        this.mediaFiles = mediaFiles;
+    }
+
+    public User getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(User instructor) {
+        this.instructor = instructor;
+    }
+}
