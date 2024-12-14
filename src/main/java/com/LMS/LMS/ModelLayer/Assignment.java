5369519ee7b85b356095f77f3e61130b14d979ed
Mohamed Taha <mhmdth276@gmail.com>
2024-12-14 14:44:10 +0200

    Completing Assginment and Quiz

--- src/main/java/com/LMS/LMS/ModelLayer/Assignment.java
+++ src/main/java/com/LMS/LMS/ModelLayer/Assignment.java
@@ -1,5 +1,7 @@
 package com.LMS.LMS.ModelLayer;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 public class Assignment {
@@ -8,44 +10,48 @@ public class Assignment {
     private Integer id;
 
     private String title;
+    private String description;
+    private LocalDateTime dueDate;
+    private Integer maxScore;
 
     @ManyToOne
     private Course course;
 
+    @OneToMany(mappedBy = "assignment", cascade = CascadeType.ALL)
+    private List<AssignmentGrades> grades;
 
-    public Assignment(Integer id, String title, Course course ) {
+    // Constructors
+    public Assignment() {}
+
+    public Assignment(Integer id, String title, String description, LocalDateTime dueDate,
+                      Integer maxScore, Course course) {
         this.id = id;
         this.title = title;
-        this.course = course;
-
-    }
-
-    public Assignment() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Course getCourse() {
-        return course;
-    }
-
-    public void setCourse(Course course) {
+        this.description = description;
+        this.dueDate = dueDate;
+        this.maxScore = maxScore;
         this.course = course;
     }
 
+    // Getters and Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDateTime getDueDate() { return dueDate; }
+    public void setDueDate(LocalDateTime dueDate) { this.dueDate = dueDate; }
+
+    public Integer getMaxScore() { return maxScore; }
+    public void setMaxScore(Integer maxScore) { this.maxScore = maxScore; }
+
+    public Course getCourse() { return course; }
+    public void setCourse(Course course) { this.course = course; }
+
+    public List<AssignmentGrades> getGrades() { return grades; }
+    public void setGrades(List<AssignmentGrades> grades) { this.grades = grades; }
 }
--- src/main/java/com/LMS/LMS/ModelLayer/QuizGrades.java
+++ src/main/java/com/LMS/LMS/ModelLayer/QuizGrades.java
@@ -1,6 +1,6 @@
 package com.LMS.LMS.ModelLayer;
-
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class QuizGrades {
@@ -14,60 +14,55 @@ public class QuizGrades {
     @ManyToOne
     private User student;
 
-    private String grades;
-
+    private String grade;
     private String feedback;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Integer attemptNumber;
+    private String answers; // Could store JSON of answers
 
+    // Constructors
+    public QuizGrades() {}
 
-    public QuizGrades(Integer id, Quiz quiz, User student, String grades , String feedback) {
+    public QuizGrades(Integer id, Quiz quiz, User student, String grade,
+                      String feedback, LocalDateTime startTime, LocalDateTime endTime,
+                      Integer attemptNumber, String answers) {
         this.id = id;
         this.quiz = quiz;
         this.student = student;
-        this.grades = grades;
+        this.grade = grade;
         this.feedback = feedback;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.attemptNumber = attemptNumber;
+        this.answers = answers;
     }
 
-    public QuizGrades() {
+    // Getters and Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    }
+    public Quiz getQuiz() { return quiz; }
+    public void setQuiz(Quiz quiz) { this.quiz = quiz; }
 
-    public Integer getId() {
-        return id;
-    }
+    public User getStudent() { return student; }
+    public void setStudent(User student) { this.student = student; }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    public String getGrade() { return grade; }
+    public void setGrade(String grade) { this.grade = grade; }
 
-    public Quiz getQuiz() {
-        return quiz;
-    }
+    public String getFeedback() { return feedback; }
+    public void setFeedback(String feedback) { this.feedback = feedback; }
 
-    public void setQuiz(Quiz quiz) {
-        this.quiz = quiz;
-    }
+    public LocalDateTime getStartTime() { return startTime; }
+    public void setStartTime(LocalDateTime startTime) { this.startTime = startTime; }
 
-    public User getStudent() {
-        return student;
-    }
+    public LocalDateTime getEndTime() { return endTime; }
+    public void setEndTime(LocalDateTime endTime) { this.endTime = endTime; }
 
-    public void setStudent(User student) {
-        this.student = student;
-    }
+    public Integer getAttemptNumber() { return attemptNumber; }
+    public void setAttemptNumber(Integer attemptNumber) { this.attemptNumber = attemptNumber; }
 
-    public String getGrades() {
-        return grades;
-    }
-
-    public void setGrades(String grades) {
-        this.grades = grades;
-    }
-
-    public String getFeedback() {
-        return feedback;
-    }
-
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
-    }
+    public String getAnswers() { return answers; }
+    public void setAnswers(String answers) { this.answers = answers; }
 }
