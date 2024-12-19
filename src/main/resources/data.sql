f33ecf20ffaadbc1994eca0f88fab4d033b19c22
unknown <wdm46857@gmail.com>
2024-12-19 19:01:01 +0200

    new updates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,8 @@ VALUES ( 'admin', 'admin123', 'ADMIN', 'admin@example.com'),
 -- Insert statements for Course
 INSERT INTO Course (title, description, duration, instructor_id)
 VALUES ( 'Java Basics', 'Introduction to Java programming', 30, 2),
-       ( 'Spring Boot Advanced', 'Deep dive into Spring Boot framework', 45, 2);
+       ( 'Spring Boot Advanced', 'Deep dive into Spring Boot framework', 45, 2),
+       ( 'Software Engineering', 'Architectural styles', 45, 2);
 --
 -- Insert statements for Lesson
 INSERT INTO lesson ( topic, course_id)
